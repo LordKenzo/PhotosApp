6587dcefb41067ff6a2882b19ca41326c65abc3a
LordKenzo <>
2019-06-03 23:30:42 +0200

    add login api

--- backend/server/controllers/user.js
+++ backend/server/controllers/user.js
@@ -1,15 +1,34 @@
 const user = require('../models').user;
 
-function create(req, res) {
+const create = (req, res) => {
     user.create(req.body)
-        .then(user => {
-            return res.status(200).send({user});
+        .then(userCreated => {
+            return res.status(200).send({ userCreated });
         })
         .catch(err => {
-            return res.status(500).send({err});
+            return res.status(500).send({ err });
         })
 }
 
+const login = async (req, res) => {
+    try {
+        const userFounded = await user.findOne({
+            where: {
+                usernsame: req.body.username,
+                password: req.body.password
+            }
+        }).catch(err => res.status(500).send({ 'error': 'Database Error - check SQL' }));
+        if (userFounded) {
+            res.status(200).send({ user: userFounded });
+        } else {
+            res.status(401).send({ message: 'Access non autorizzato' })
+        }
+    } catch (e) {
+        res.status(500).send({ 'error': 'Errore richiesta' })
+    }
+}
+
 module.exports = {
-    create
+    create,
+    login
 };
--- backend/server/routes/user.js
+++ backend/server/routes/user.js
@@ -2,4 +2,5 @@ const userController = require('../controllers').user;
 
 module.exports = (app) => {
     app.post('/api/user', userController.create);
+    app.post('/api/login', userController.login);
 }
